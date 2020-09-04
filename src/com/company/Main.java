8543fc9f4c0e73f340f967bcf1a40f448d922a82
lupa <sechinkirill@yandex.ru>
2020-09-04 13:43:39 +0300

    start project

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String s = "The weather was fine";
+        System.out.println(s);
+
+    }
+}
